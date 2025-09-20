4f96aedad515ea65c1be0b808727a4c190586876
Ayşe Beyza Gür <beyyzgur@gmail.com>
2025-09-20 17:34:57 +0300

    feat: add ToDosApp - SQLite

---
+++ studies/ToDosApp-SQLite/ToDosApp/ui/screens/SaveScreen.swift
@@ -0,0 +1,64 @@
+//
+//  SaveScreen.swift
+//  ToDosApp
+//
+//  Created by Kasım on 6.09.2025.
+//
+
+import SwiftUI
+
+struct SaveScreen: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var randomImage = ""
+    @State private var nameController = ""
+    @State private var showError = false
+    var viewModel = SaveViewModel()
+    
+    var body: some View {
+        VStack(spacing: 32) {
+            Image(randomImage)
+            
+            TextField("Name",text: $nameController).textFieldStyle(CustomTextfieldStyle())
+            
+            if showError {
+                Text("Name can not be empty !").foregroundStyle(AppColors.red)
+            }
+            
+            Button{
+                if nameController.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                    showError = true
+                }else{
+                    showError = false
+                    Task {
+                        await viewModel.save(name: nameController, image: randomImage)
+                    }
+                    dismiss()
+                }
+            }label: {
+                Text("Save").frame(maxWidth: .infinity)
+            }.buttonStyle(CustomButtonStyle())
+        }.frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .top)
+            .padding()
+        .navigationTitle("Save Screen")
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button {
+                        dismiss()
+                    } label: {
+                        HStack {
+                            Image(systemName: "arrow.backward").foregroundColor(AppColors.white)
+                            Text("ToDos").foregroundColor(AppColors.white)
+                        }
+                    }
+                }
+            }.onAppear(){
+                let images = ["agac","araba","cicek","damla","gezegen","gunes","roket","semsiye","yildiz"]
+                randomImage = images.randomElement() ?? "agac"
+            }
+    }
+}
+
+#Preview {
+    SaveScreen()
+}
---
+++ studies/ToDosApp-SQLite/ToDosApp/ui/viewmodel/MainViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  MainViewModel.swift
+//  ToDosApp
+//
+//  Created by Kasım on 11.09.2025.
+//
+
+import Foundation
+
+@MainActor
+class MainViewModel : ObservableObject {
+    private let repository = ToDosRepository()
+    @Published var toDosList = [ToDos]()
+    
+    func loadToDos() async {
+        do{
+            toDosList = try await repository.loadToDos()
+        }catch{
+            toDosList = [ToDos]()
+        }
+    }
+    
+    func search(searchText:String) async {
+        do{
+            toDosList = try await repository.search(searchText: searchText)
+        }catch{
+            toDosList = [ToDos]()
+        }
+    }
+    
+    func delete(id:Int) async {
+        do{
+            try await repository.delete(id: id)
+            await loadToDos()
+        }catch{
+            
+        }
+    }
+}
